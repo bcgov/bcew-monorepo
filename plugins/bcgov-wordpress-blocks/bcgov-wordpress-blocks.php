70a49ed0892511985769e1a3a936eb4f0212157a
robrien-bcgov <richard.obrien@gov.bc.ca>
2026-04-16 07:54:02 -0700

    DSWP-716 Proof-of-concept: hero image - 1 file - variation

--- plugins/bcgov-wordpress-blocks/bcgov-wordpress-blocks.php
+++ plugins/bcgov-wordpress-blocks/bcgov-wordpress-blocks.php
@@ -17,6 +17,72 @@
 if ( ! defined( 'ABSPATH' ) ) {
 	exit; // Exit if accessed directly.
 }
+
+/**
+ * Adds a block variation on the core/cover block to be used as the default variation when inserting a cover block in the editor. This allows us to set custom default attributes for the cover block, such as a custom background color, and to use a custom template for the block variation.
+ *
+ * @see https://developer.wordpress.org/block-editor/reference-guides/block-api/block-variations/#registering-block-variations
+ * @see https://developer.wordpress.org/block-editor/reference-guides/block-api/block-variations/#block-variation-attributes
+ * @see https://developer.wordpress.org/block-editor/reference-guides/block-api/block-variations
+ * @see https://developer.wordpress.org/block-editor/reference-guides/block-api/block-variations/#block-variation-template
+ * @see https://developer.wordpress.org/block-editor/reference-guides/block-api/block-variations/#setting-a-default-block-variation
+ * @return void
+ */
+add_filter( 'get_block_type_variations', 'custom_cover_variation', 10, 2 );
+function custom_cover_variation( $variations, $block_type ) {
+	// Only modify variations for the cover block.
+	if ( 'core/cover' !== $block_type->name ) {
+		return $variations;
+	}
+
+	// Add a custom variation.
+	$variations[] = [
+		'name'        => 'hero-image',
+		'title'       => __( 'Hero Image', 'textdomain' ),
+		'description' => __( 'A Hero Image variation on the cover block', 'textdomain' ),
+		'scope'       => [ 'inserter' ],
+		'isDefault'   => false,
+		'attributes'  => [
+			'align'              => 'full',
+			'layout'             => [
+				'type'        => 'constrained',
+				'contentSize' => '468px',
+			],
+		],
+		'innerBlocks' => [
+			[
+				'core/group',
+				[
+					'layout' => [
+						'type'        => 'constrained',
+						'contentSize' => '468px',
+					],
+					'style'  => [
+						'color'  => [
+							'background' => '#013366B3',
+						],
+						'border' => [
+							'left' => [
+								'width' => '0.5rem',
+								'color' => '#fcba19',
+								'style' => 'solid',
+							],
+						],
+					],
+				],
+				[
+					[ 'core/heading', [ 'level' => 1, 'content' => 'Hero title', 'style' => [ 'color' => [ 'text' => '#ffffff' ] ] ] ],
+					[ 'core/paragraph', [ 'content' => 'Hero description', 'style' => [ 'color' => [ 'text' => '#ffffff' ] ] ] ],
+					[ 'core/paragraph', [ 'content' => 'Visit <a href="/some-page">another page</a>', 'style' => [ 'color' => [ 'text' => '#ffffff' ] ] ] ],
+				],
+			],
+		],
+		'icon' => 'star-filled',
+	];
+
+	return $variations;
+}
+
 /**
  * Registers the block(s) metadata from the `blocks-manifest.php` and registers the block type(s)
  * based on the registered block metadata. Behind the scenes, it registers also all assets so they can be enqueued
