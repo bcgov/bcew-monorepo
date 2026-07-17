95046f1b7c381d9ad514583827c5e996e4dc3f42
Michael Haswell <Michael.Haswell@gov.bc.ca>
2026-07-17 13:25:37 -0700

    Remove unused build files, lint files, etc. Update dependencies to use catalog versions of WordPress packages instead of specific versions

--- plugins/wordpress-document-repository/build/css/index.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array(), 'version' => '94fde8bc20e50c10a8d6');
--- plugins/wordpress-document-repository/build/metadata-settings/index.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-api-fetch', 'wp-components', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => 'f499fe5560b3ce6196ca');
