a58fa8cd0a720fb43adec2b86eaae2d9df1ffb12
robrien-bcgov <richard.obrien@gov.bc.ca>
2026-08-10 14:36:36 -0700

    dswp-1036 revert to main

--- plugins/bcew-chefs-embed/playwright.config.js
+++ plugins/bcew-chefs-embed/playwright.config.js
@@ -5,7 +5,6 @@ const config = defineConfig( {
     ...baseConfig,
     testDir: 'tests/e2e',
     workers: 1,
-    globalSetup: require.resolve( './tests/e2e/global-setup.js' ),
 } );
 
 export default config;
