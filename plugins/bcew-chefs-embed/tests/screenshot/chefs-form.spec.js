1869b7d6a8d5b4f0422bc1c4954fe5561623d958
robrien-bcgov <richard.obrien@gov.bc.ca>
2026-08-13 15:26:55 -0700

    dswp-1133 show chefs form in block editor

--- plugins/bcew-chefs-embed/tests/screenshot/chefs-form.spec.js
+++ plugins/bcew-chefs-embed/tests/screenshot/chefs-form.spec.js
@@ -1,19 +1,41 @@
 const { test, expect } = require( '@wordpress/e2e-test-utils-playwright' );
 
+const BLOCK_NAME = 'bcew-chefs-embed/chefs-form';
+
 test.describe( 'chefs-form block', () => {
     test( 'should be visible', async ( { admin, editor } ) => {
         await admin.createNewPost();
 
         await editor.insertBlock( {
-            name: 'bcew-chefs-embed/chefs-form',
+            name: BLOCK_NAME,
         } );
 
         const block = editor.canvas.locator(
-            '[data-type="bcew-chefs-embed/chefs-form"]'
+            `[data-type="${ BLOCK_NAME }"]`
         );
 
         await expect( block ).toBeVisible();
 
         await expect( block ).toHaveScreenshot();
+    } );
+
+    test( 'shows placeholder when no form is selected', async ( {
+        admin,
+        editor,
+    } ) => {
+        await admin.createNewPost();
+
+        await editor.insertBlock( {
+            name: BLOCK_NAME,
+        } );
+
+        const block = editor.canvas.locator(
+            `[data-type="${ BLOCK_NAME }"]`
+        );
+
+        await expect( block ).toBeVisible();
+        await expect( block ).toHaveScreenshot(
+            'chefs-form-block-placeholder-state.png'
+        );
     } );
 } );
