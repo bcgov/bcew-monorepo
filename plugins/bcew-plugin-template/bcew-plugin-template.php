49c9088e060ed86ca8a3fa9f1c501559f90101c0
ASpiteri-BCGov <49036255+ASpiteri-BCGov@users.noreply.github.com>
2026-09-24 15:07:48 -0700

    Update bcew-plugin-template.php

--- plugins/bcew-plugin-template/bcew-plugin-template.php
+++ plugins/bcew-plugin-template/bcew-plugin-template.php
@@ -32,13 +32,13 @@ if ( file_exists( $bcew_plugin_template_autoload ) ) {
 	require_once $bcew_plugin_template_autoload;
 } else {
 	spl_autoload_register(
-		static function ( $class ) {
+		static function ( $class_name ) {
 			$prefix = 'Bcgov\\Bcew\\PluginTemplate\\';
-			if ( 0 !== strpos( $class, $prefix ) ) {
+			if ( 0 !== strpos( $class_name, $prefix ) ) {
 				return;
 			}
 
-			$relative = substr( $class, strlen( $prefix ) );
+			$relative = substr( $class_name, strlen( $prefix ) );
 			$path     = __DIR__ . '/src/' . str_replace( '\\', '/', $relative ) . '.php';
 
 			if ( is_readable( $path ) ) {
