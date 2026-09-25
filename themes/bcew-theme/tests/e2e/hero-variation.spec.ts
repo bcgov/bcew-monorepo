f151a38f3a81692fa2bd0e6bddf2f8ed535bf8f7
robrien-bcgov <richard.obrien@gov.bc.ca>
2026-09-25 10:57:56 -0700

    feat(DSWP-1225): https://citz-gdx.atlassian.net/browse/DSWP-1225 add outline hover fallback

--- themes/bcew-theme/tests/e2e/hero-variation.spec.ts
+++ themes/bcew-theme/tests/e2e/hero-variation.spec.ts
@@ -80,6 +80,7 @@ test( 'test that we can create a Hero Image block with all fields filled', async
         .frameLocator( 'iframe[name="editor-canvas"]' )
         .locator( '.editor-styles-wrapper' );
     await editorContent.waitFor();
+    await editorContent.evaluate( async () => document.fonts.ready );
     await expect( editorContent ).toHaveScreenshot(
         'hero-image-all-fields-editor.png',
         { animations: 'disabled' }
@@ -141,6 +142,7 @@ test( 'test that we can create a Hero Image block with only a title', async ( {
         .frameLocator( 'iframe[name="editor-canvas"]' )
         .locator( '.editor-styles-wrapper' );
     await editorContentTitleOnly.waitFor();
+    await editorContentTitleOnly.evaluate( async () => document.fonts.ready );
     await expect( editorContentTitleOnly ).toHaveScreenshot(
         'hero-image-title-only-editor.png',
         { animations: 'disabled' }
